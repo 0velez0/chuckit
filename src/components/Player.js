088b44c7ccc59904fb888bc23cfa5545b62f9db9
0velez0 <mariaivelez@gmail.com>
2018-07-25 15:23:30 -0700

    modify to only have one player name input

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,28 +1,78 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Button
+} from "react-native";
+import Homescreen from "./Homescreen";
+import Leaderboard from ".Leaderboard";
 
 export default class Player extends React.Component {
-  state = {
-    playerName: ''
+  constructor() {
+    super();
+    this.state = {
+      playerName: ""
+    };
   }
 
-  playerNameChangedHandler = (value) => {
+  // TODO I know I need to track state of score
+
+  onPressPlayGameHandler = () => {
+    this.props.navigator.push({
+      screen: "GamePlay"
+    });
+  };
+
+  playerNameChangedHandler = value => {
+    console.log(value);
     this.setState({
       playerName: value
     });
   };
 
+  playerSubmitHandler = () => {
+    // Doing this because I don't want to allow user to add an empty playerName
+    if (this.state.playerName.trim() === "") {
+      return;
+    }
 
+    // lets you check the validity of the name inside the playerInput component
+    this.props.onPlayerAdded(this.state.playerName);
+  };
+
+  // put your name
+  // okay button with callback
   render() {
     return (
       <View style={styles.container}>
-      <TextInput
-      style={styles.textStyle}
-      placeholder="player name"
-      value={this.state.playerName}
-      onChangeText={this.playerNameChangedHandler}
-      />
+        <TextInput
+          style={styles.textStyle}
+          placeholder="player name"
+          value={this.state.playerName}
+          onChangeText={this.playerNameChangedHandler}
+        />
 
+        <Button
+          title="Add"
+          style={styles.playerButton}
+          onPress={this.playerSubmitHandler}
+          // Above can be this instead:
+          // onPress={() => this.onPlayerAdded(this.state.playerName)}
+        />
+
+        <View>
+          <TouchableOpacity
+            onPress={this.onPressPlayGameHandler}
+            style={styles.playGameButton}
+          >
+            <Text> PLAY GAME </Text>
+          </TouchableOpacity>
+        </View>
+
+        <GamePlay playerName={this.state.playerName} />
       </View>
     );
   }
@@ -31,9 +81,9 @@ export default class Player extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
   textStyle: {
     width: 200,
@@ -41,5 +91,17 @@ const styles = StyleSheet.create({
     borderColor: "black",
     borderWidth: 1
   },
-
+  inputContainer: {
+    // flex: 1,
+    width: "90%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
+  },
+  onPressPlayGameHandler: {
+    borderWidth: 1,
+    padding: 20,
+    borderColor: "black",
+    backgroundColor: "#d5dfdc"
+  }
 });
