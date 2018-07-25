5fa3c90c77061ca685a2bfaf8a038cdd5c08d4a5
0velez0 <mariaivelez@gmail.com>
2018-07-24 17:25:46 -0700

    componentDidMount and initial firebase ref

--- App.js
+++ App.js
@@ -6,12 +6,6 @@ import { Navigation } from "react-native-navigation";
 Navigation.registerComponent("Homescreen", () => Homescreen);
 Navigation.registerComponent("GamePlay", () => GamePlay);
 
-// TODO pin topnav to top of screen when hit pause
-// // TODO: use Firebase
-// // TODO: add players
-// // TODO: add Leaderboard screen
-// // TODO put only score after game over
-
 export default class App extends React.Component {
   render() {
     return <Homescreen />;
--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -1,5 +1,7 @@
 import React from "react";
 import firebase from "firebase";
+import { API_KEY, PROJECT_ID } from "react-native-dotenv";
+import Player from "./Player";
 
 import {
   Text,
@@ -12,15 +14,6 @@ import {
 
 import garbageTruck from "../../assets/images/trash/garbage-truck.png";
 
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "<your-api-key>",
-  authDomain: "<your-auth-domain>",
-  databaseURL: "<your-database-url>",
-  storageBucket: "<your-storage-bucket>"
-};
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-
 export default class Homescreen extends React.Component {
   onPressPlayGameHandler = () => {
     this.props.navigator.push({
@@ -28,17 +21,48 @@ export default class Homescreen extends React.Component {
     });
   };
 
-  fetch('https://chuckit-a6727.firebaseio.com/', {
-  method: 'POST',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    firstParam: 'name',
-    secondParam: 'value',
-  }),
-});
+  componentDidMount() {
+    // put fetch data function in here
+    const config = {
+      apiKey: API_KEY,
+      authDomain: "chuckit-a6727.firebaseapp.com",
+      databaseURL: "https://chuckit-a6727.firebaseio.com",
+      projectId: PROJECT_ID,
+      storageBucket: "chuckit-a6727.appspot.com",
+      messagingSenderId: "933419915503"
+    };
+    firebase.initializeApp(config);
+    console.log(firebase);
+    firebase
+      .database()
+      .ref("players/001")
+      .set({
+        name: "Ada",
+        score: 3
+      })
+      .then(() => {
+        console.log("I GOT IN THERE!");
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  // const myRequest = new Request('https://chuckit-a6727.firebaseio.com/');
+  //
+  // const myURL = myRequest.url;
+
+  //   fetch('https://chuckit-a6727.firebaseio.com/', {
+  //   method: 'POST',
+  //   headers: {
+  //     Accept: 'application/json',
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify({
+  //     firstParam: 'name',
+  //     secondParam: 'value',
+  //   }),
+  // });
 
   render() {
     return (
